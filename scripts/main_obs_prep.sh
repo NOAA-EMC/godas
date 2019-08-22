f163f3b0250cf1a1d36f85d136df56e394e64aeb
Stylianos.Flampouris <Stylianos.Flampouris@noaa.gov>
2019-08-22 18:56:20 +0000

    Add SST from WindSat

--- scripts/main_obs_prep.sh
+++ scripts/main_obs_prep.sh
@@ -3,8 +3,9 @@ echo 'main_obs_prep.sh starts'
 echo ${ROOT_GODAS_DIR}
 echo CDATE is $CDATE
 
-source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh 
-source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
+#source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh 
+#source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
+source ${ROOT_GODAS_DIR}/scripts/sst_windsat_prep_obs.sh
 
 #${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh
 
---
+++ scripts/sst_windsat_prep_obs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l 
+
+cd $DCOMROOT
+
+OBSDCOM=$DCOMROOT/sst.windsat_l3u.ghrsst/$PDY
+if [ -d "$OBSDCOM" ]; then
+   
+   OUTDIR=${ROTDIR}/${CDATE}
+   mkdir -p ${OUTDIR}
+   
+   cd $OBSDCOM
+   echo SST Observations from WindSat for $PDY exist at `pwd`
+   
+   s="${IODA_EXEC}/gds2_sst2ioda.py -i "
+   for files in `ls *.nc`; do
+      s+=" $OBSDCOM/${files} "
+   done
+   
+   s+=" -o ${OUTDIR}/ioda.sst.${DA_SLOT_LEN}h.nc -d ${CDATE}"
+
+   eval ${s}
+
+else
+   
+   set -x
+   echo There are no SST observations from WindSat for ${CDATE}  
+   set +x
+
+fi
+ 
