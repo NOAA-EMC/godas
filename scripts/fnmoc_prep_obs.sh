3d98a754a7339cddbe9a8c4930f4cb002f706838
flampouris <stylianos.flampouris@noaa.gov>
2019-08-22 17:23:29 -0400

    Pull request (#8)
    * Updated adt_prep_obs.sh; it goes through the conversion and dumps the netcdf file as expected
    
    * Add the script for fnmoc data
    
    * Update the module used for anaconda, there is an issue with Stelios setup
    
    * Error with anaconda loading is identified
    
    * jobs/JJOB_OBS_PREP
    
    * Add SST from WindSat
    
    * Comments deleted

---
+++ scripts/fnmoc_prep_obs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+
+echo fnmoc_prep_obs.sh starts
+
+#set -x
+
+cd $DCOMROOT
+
+OBSDCOM=$DCOMROOT/insitu.fnmoc/$PDY
+   
+OUTDIR=${ROTDIR}/${CDATE}
+mkdir -p ${OUTDIR}
+   
+cd $OBSDCOM
+   
+for datafilename in profile ship trak
+do
+   datafile="$OBSDCOM/${PDY}00.${datafilename}"
+   
+   if [ -f "$datafile" ]; then
+
+      echo "$datafile" from FNMOC for $PDY exist at `pwd`
+
+      s="${IODA_EXEC}/godae_${datafilename}2ioda.py -i " 
+      s+=" ${datafile}"
+      s+=" -o ${OUTDIR}/ioda.${datafilename}.${DA_SLOT_LEN}h.nc -d ${CDATE}"
+      eval ${s}
+   
+   else
+      set -x
+      echo There are no $datafile observations for ${CDATE}  
+      set +x
+   fi
+done
+
+echo fnmoc_prep_obs.sh ends
+
