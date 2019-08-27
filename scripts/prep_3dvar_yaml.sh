059944c069a2bd31ac8994e0180dc219f334c95c
Stylianos.Flampouris <Stylianos.Flampouris@noaa.gov>
2019-08-27 23:23:58 +0000

    Cleanup of JJOB_DA_PREP and initial commit of letkf related scripts

---
+++ scripts/prep_3dvar_yaml.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # 
+#                       Unix Script Documentation 
+# 
+# Script Name        :
+#
+# Script Description :
+#
+# Details            :
+#
+# Author             :
+#
+# History Log        :
+#
+# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # 
+#set -x
+
+echo '#=================================================================#'
+echo '#                    prep_3dvar_yaml.sh starts                    #'
+echo '#                                                                 #'
+
+while getopts "i:d:" opt; do
+   case $opt in
+      i) yamlfile=("$OPTARG");;
+   esac
+done
+shift $((OPTIND -1))
+
+cd ${RUNDIR}
+
+## Set date for 3DVAR
+sed -i "s/WINDOW_BEGIN/${window_begin}/g" ${yamlfile}
+sed -i "s/WINDOW_LENGTH/${window_length}/g" ${yamlfile}
+sed -i "s/BKG_DATE/${bkg_date}/g" ${yamlfile}
+#
+## Setup Jo cost function
+##-----------------------
+mkdir -p Data
+obsdatabase=${ROTDIR}/${CDATE} # From obs_prep jjob
+echo "obsdatabase="$obsdatabase
+# Add adt obs to Jo
+obsfile=$obsdatabase/ioda.adt.${DA_SLOT_LEN}h.nc 
+if [ -f $obsfile ]; then
+   echo "Adding ADT to Jo cost function"
+   ln -sf ${obsfile} ./Data/adt.nc
+   sed -e '/ADT_JO/{r ${RUNDIR}/yaml/adt.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
+   cp 3dvartmp.yml ${yamlfile}
+   rm 3dvartmp.yml
+else
+   echo "Not assimilating ADT"
+fi
+
+# Add sst obs to Jo
+obsfile=$obsdatabase/ioda.sst.${DA_SLOT_LEN}h.nc 
+if [ -f $obsfile ]; then
+   echo "Adding SST to Jo cost function"
+   ln -sf ${obsfile} ./Data/sst.nc
+   sed -e '/SST_JO/{r ${RUNDIR}/yaml/sst.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
+   cp 3dvartmp.yml ${yamlfile}
+   rm 3dvartmp.yml
+else
+  echo "Not assimilating SST"
+fi
+
+# Add adt insitu profiles to Jo
+obsfile=$obsdatabase/ioda.profile.${DA_SLOT_LEN}h.nc 
+if [ -f $obsfile ]; then
+   echo "Adding Profiles to Jo cost function"
+   ln -sf ${obsfile} ./Data/prof.nc
+   sed -e '/INSITU_JO/{r ${RUNDIR}/yaml/profile.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
+   cp 3dvartmp.yml ${yamlfile}
+   rm 3dvartmp.yml
+else
+   echo "Not assimilating profiles"
+fi
+#
+echo '#                                                                 #'
+echo '#                      prep_3dvar_yaml.sh ends                    #'
+echo '#=================================================================#'
