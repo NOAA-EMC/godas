97d537c35abbe35f7b293928e9c683229ec96707
Stylianos.Flampouris <Stylianos.Flampouris@noaa.gov>
2019-08-29 17:19:49 +0000

    generalized scripts

--- scripts/prep_3dvar_yaml.sh
+++ scripts/prep_3dvar_yaml.sh
@@ -14,7 +14,7 @@
 # History Log        :
 #
 # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # 
-#set -x
+set -x
 
 echo '#=================================================================#'
 echo '#                    prep_3dvar_yaml.sh starts                    #'
@@ -23,6 +23,7 @@ echo '#                                                                 #'
 while getopts "i:d:" opt; do
    case $opt in
       i) yamlfile=("$OPTARG");;
+      d) RUNDIR=("$OPTARG");;
    esac
 done
 shift $((OPTIND -1))
@@ -36,45 +37,11 @@ sed -i "s/BKG_DATE/${bkg_date}/g" ${yamlfile}
 #
 ## Setup Jo cost function
 ##-----------------------
-mkdir -p Data
-obsdatabase=${ROTDIR}/${CDATE} # From obs_prep jjob
-echo "obsdatabase="$obsdatabase
-# Add adt obs to Jo
-obsfile=$obsdatabase/ioda.adt.${DA_SLOT_LEN}h.nc 
-if [ -f $obsfile ]; then
-   echo "Adding ADT to Jo cost function"
-   ln -sf ${obsfile} ./Data/adt.nc
-   sed -e '/ADT_JO/{r ${RUNDIR}/yaml/adt.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
-   cp 3dvartmp.yml ${yamlfile}
-   rm 3dvartmp.yml
-else
-   echo "Not assimilating ADT"
-fi
 
-# Add sst obs to Jo
-obsfile=$obsdatabase/ioda.sst.${DA_SLOT_LEN}h.nc 
-if [ -f $obsfile ]; then
-   echo "Adding SST to Jo cost function"
-   ln -sf ${obsfile} ./Data/sst.nc
-   sed -e '/SST_JO/{r ${RUNDIR}/yaml/sst.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
-   cp 3dvartmp.yml ${yamlfile}
-   rm 3dvartmp.yml
-else
-  echo "Not assimilating SST"
-fi
+${ROOT_GODAS_DIR}/scripts/SetupJoCostFunction.sh \
+      -i ${yamlfile}                             \
+      -d $RUNDIR
 
-# Add adt insitu profiles to Jo
-obsfile=$obsdatabase/ioda.profile.${DA_SLOT_LEN}h.nc 
-if [ -f $obsfile ]; then
-   echo "Adding Profiles to Jo cost function"
-   ln -sf ${obsfile} ./Data/prof.nc
-   sed -e '/INSITU_JO/{r ${RUNDIR}/yaml/profile.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
-   cp 3dvartmp.yml ${yamlfile}
-   rm 3dvartmp.yml
-else
-   echo "Not assimilating profiles"
-fi
-#
 echo '#                                                                 #'
 echo '#                      prep_3dvar_yaml.sh ends                    #'
 echo '#=================================================================#'
