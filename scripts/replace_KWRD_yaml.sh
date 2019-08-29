4d543fdac960b91f89fc07fa113f2c24b13bd8d8
Stylianos.Flampouris <Stylianos.Flampouris@noaa.gov>
2019-08-29 23:42:35 +0000

    Add the genensper yaml and the replace_KWRD_yaml.sh

---
+++ scripts/replace_KWRD_yaml.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # 
+#                       Unix Script Documentation 
+# 
+# Script Name        :
+#
+# Script Description :
+#
+# Details            :
+#
+# Author             :
+#
+# History Log        :
+#
+# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # 
+# set -x
+
+echo '#=================================================================#'
+echo '#                   Replace_Keyword_yaml.sh starts                #'
+echo '#                                                                 #'
+
+while getopts "i:k:v:" opt; do
+   case $opt in
+      i) yamlfile=("$OPTARG");;
+      k) keyword=("$OPTARG");;
+      v) value=(${OPTARG});;
+   esac
+done
+shift $((OPTIND -1))
+
+cd ${RUNDIR}
+
+if [ -f "$yamlfile" ]; then
+
+   sed -i "s/${keyword}/${value}/g"  ${yamlfile}
+
+else
+   echo "ERROR: There is not "$yamlfile "exiting..."
+   #exit
+fi
+
+echo '#                                                                 #'
+echo '#                    Replace_Keyword_yaml.sh                      #'
+echo '#=================================================================#'
