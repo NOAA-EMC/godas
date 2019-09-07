7b77d493cfbfb5fd90606c9fadab629c48fa6188
Stylianos.Flampouris <Stylianos.Flampouris@noaa.gov>
2019-09-07 20:40:59 +0000

    The JJOB and yaml files for H(x) of the ensemble are generated correctly

--- scripts/main_obs_prep.sh
+++ scripts/main_obs_prep.sh
@@ -15,7 +15,6 @@ for SSTsource in $ListOfSST;do
                      -i ${SSTsource}
 done
 
-
 # Testing the following
 #${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh
 #source ${ROOT_GODAS_DIR}/scripts/sst_windsat_prep_obs.sh
