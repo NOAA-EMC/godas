7b6f76c84bf8fade3f00f42281410c5e094d82aa
Stylianos.Flampouris <Stylianos.Flampouris@noaa.gov>
2019-09-07 14:25:28 +0000

    All SST can be processed

--- scripts/main_obs_prep.sh
+++ scripts/main_obs_prep.sh
@@ -5,7 +5,18 @@ echo CDATE is $CDATE
 
 source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh 
 source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
-source ${ROOT_GODAS_DIR}/scripts/sst_windsat_prep_obs.sh
+#source ${ROOT_GODAS_DIR}/scripts/sst_windsat_prep_obs.sh
+
+#All the SST obs processed
+ListOfSST="sst.windsat_l3u.ghrsst sst.amsr2_l3u.ghrsst \
+           sst.avhrr_l3u.nesdis sst.gmi_l3u.ghrsst \
+           sst.viirs_l3u.nesdis "
+
+for SSTsource in $ListOfSST;do
+   ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
+                     -i ${SSTsource}
+done
+
 
 #${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh
 
