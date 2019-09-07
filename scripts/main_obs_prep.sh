7e1bab79e87410cfb5d78e751fe867970929e73d
flampouris <stylianos.flampouris@noaa.gov>
2019-09-07 10:13:52 -0400

    Revert "General script for all SST"

--- scripts/main_obs_prep.sh
+++ scripts/main_obs_prep.sh
@@ -5,21 +5,8 @@ echo CDATE is $CDATE
 
 source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh 
 source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
+source ${ROOT_GODAS_DIR}/scripts/sst_windsat_prep_obs.sh
 
-ListOfSST="sst.windsat_l3u.ghrsst sst.amsr2_l3u.ghrsst \
-           sst.avhrr_l3u.nesdis sst.gmi_l3u.ghrsst \
-           sst.viirs_l3u.nesdis "
-
-for SSTsource in $ListOfSST;do
-   ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
-                     -i ${SSTsource}
-done
-
-
-# Testing the following
 #${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh
-#source ${ROOT_GODAS_DIR}/scripts/sst_windsat_prep_obs.sh
-#${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
-      -i sst.windsat_l3u.ghrsst
 
 echo main_obs_prep ends
