4ed8d452307c187b8ab9864127729df3fc1c74fd
Stylianos.Flampouris <Stylianos.Flampouris@noaa.gov>
2019-09-10 16:15:42 +0000

    Checking if the ADT IODA obs exist and coppies them to the Rrundir/Data If the ADT IODA obs do not exist but the raw data exist, it processes them and dumping them at the rundir/data

--- scripts/adt_prep_obs.sh
+++ scripts/adt_prep_obs.sh
@@ -2,30 +2,41 @@
 
 cd $DCOM_ROOT
 
-OBSDCOM=$DCOM_ROOT/adt.nesdis/$PDY
+OBSDCOM=$DCOM_ROOT/adt.nesdis/$PDY              #FullPath of raw obs
+OUTFILE=ioda.adt.${DA_SLOT_LEN}h.nc             #Filename of the processed obs
+PREPROCobs=${IODA_ROOT}/${CDATE}/${OUTFILE}     #FullPath/Filename of preprocessed obs
+PROCobs=${ObsRunDir}/${OUTFILE}                 #FullPath/Filename of observations to be ingested
+
+#Check if the observations have been preprocessed.
+if [ -f "${PREPROCobs}" ]; then
+   echo
+   echo PreProcessed Observations are copied from "${PREPROCobs}" \
+        to ${PROCobs}
+   echo
+
+   cp -rf ${PREPROCobs} ${PROCobs}
+
+   return
+fi
+
+# Check if the raw observations exist and process.
 if [ -d "$OBSDCOM" ]; then
-   
-   OUTDIR=${ROTDIR}/${CDATE}
-   mkdir -p ${OUTDIR}
-   
+    
    cd $OBSDCOM
-   echo ADT Observations for $PDY exist at `pwd`
+   
+   echo ADT Observations for $PDY will be processed, obs directory: `pwd` 
    
    s="${IODA_EXEC}/rads_adt2ioda.py -i "
    for files in `ls *.nc`; do
       s+=" $OBSDCOM/${files} "
    done
    
-   s+=" -o ${OUTDIR}/ioda.adt.${DA_SLOT_LEN}h.nc -d ${CDATE}"
+   s+=" -o ${PROCobs} -d ${CDATE}"
    
-   set -x
    eval ${s}
-   set +x
+
 else
    
-   set -x
    echo There are no ADT observations for ${CDATE}  
-   set +x
 
 fi
- 
--- scripts/main_obs_prep.sh
+++ scripts/main_obs_prep.sh
@@ -3,17 +3,21 @@ echo 'main_obs_prep.sh starts'
 echo ${ROOT_GODAS_DIR}
 echo CDATE is $CDATE
 
+ObsRunDir=$RUNDIR/Data/${CDATE}    #Path for observations to be ingested by DA
+mkdir -p ${ObsRunDir}
+
 source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh 
-source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
 
-ListOfSST="sst.windsat_l3u.ghrsst sst.amsr2_l3u.ghrsst \
-           sst.avhrr_l3u.nesdis sst.gmi_l3u.ghrsst \
-           sst.viirs_l3u.nesdis "
-
-for SSTsource in $ListOfSST;do
-   ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
-                     -i ${SSTsource}
-done
+#source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
+#
+#ListOfSST="sst.windsat_l3u.ghrsst sst.amsr2_l3u.ghrsst \
+#           sst.avhrr_l3u.nesdis sst.gmi_l3u.ghrsst \
+#           sst.viirs_l3u.nesdis "
+#
+#for SSTsource in $ListOfSST;do
+#   ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
+#                     -i ${SSTsource}
+#done
 
 
 # Testing the following
