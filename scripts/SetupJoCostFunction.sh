8bf4ceda48e19aff63c8b52d9774b44fbc77ca4a
guillaumevernieres <guillaume.vernieres@noaa.gov>
2019-10-10 19:20:22 -0400

    Addition of AVHRR19, AVHRRMTA and EMC ice fraction to Jo (#77)
    * Addition of avhrr19 avhrrmta and emc ice concentration
    
    * prep script for icec
    
    * adjusted ice file name
    
    * 2 cycles

--- scripts/SetupJoCostFunction.sh
+++ scripts/SetupJoCostFunction.sh
@@ -35,6 +35,7 @@ mkdir -p ${DATADIR}
 
 obsdatabase=${DATADIR}/${CDATE} 
 echo "obsdatabase="$obsdatabase
+
 # Add adt obs to Jo
 obsfile=$obsdatabase/ioda.adt.${DA_SLOT_LEN}h.nc
 echo $obsfile
@@ -48,32 +49,76 @@ else
    echo "Not assimilating ADT"
 fi
 
-# Add ghrsst to Jo.
-# HACK: sst preproc obs lost inst name ...
-obsfile=$obsdatabase/ioda.sst.${DA_SLOT_LEN}h.nc 
+# Add icec obs to Jo (f285-f286 SSMI/SSMIS)
+obsfile=$obsdatabase/ioda.icec.cat_l2.emc.${DA_SLOT_LEN}h.nc
 echo $obsfile
-echo "Adding $inst SST to Jo cost function"
-ln -sf ${obsfile} ${DATADIR}/ioda.sst.windsat_l3u.ghrsst.nc
-sed -e '/SST_windsat_JO/{r '${RUNDIRC}'/yaml/sst.windsat_l3u.ghrsst.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml                                                                                           
-cp 3dvartmp.yml ${yamlfile}                                                                                                                                                                              
-rm 3dvartmp.yml
+if [ -f $obsfile ]; then
+   echo "Adding ice concentration to Jo cost function"
+   # TODO: Subsample elsewhere
+   cp ${obsfile} ${obsdatabase}/ioda.icec.cat_l2.emc_LARGE.nc
+   module load nco
+   # Create record dim
+   ncks --mk_rec_dmn nlocs ${obsdatabase}/ioda.icec.cat_l2.emc_LARGE.nc ${obsdatabase}/icec-tmp.nc
+   # Subsample
+   ncks -F -d nlocs,1,,50 ${obsdatabase}/icec-tmp.nc ${DATADIR}/ioda.icec.cat_l2.emc.nc
+   rm ${obsdatabase}/sst-tmp.nc
+   rm ${obsdatabase}/ioda.sst.${sst_source}_LARGE.nc
 
-#ListOfGHRSST="sst windsat gmi amsr2"
-#for inst in $ListOfGHRSST; do
-#   obsfile=$obsdatabase/ioda.sst.${inst}_l3u.ghrsst.${DA_SLOT_LEN}h.nc 
-#   echo $obsfile
-#   if [ -f $obsfile ]; then
-#      echo "Adding $inst SST to Jo cost function"
-#      ln -sf ${obsfile} ${DATADIR}/ioda.sst.${inst}_l3u.ghrsst.nc
-#      echo SST_${inst}_JO
-#      echo sst.${inst}_l3u.ghrsst.yml
-#      sed -e '/SST_'${inst}'_JO/{r '${RUNDIRC}'/yaml/sst.'${inst}'_l3u.ghrsst.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
-#      cp 3dvartmp.yml ${yamlfile}
-#      rm 3dvartmp.yml
-#   else
-#     echo "Not assimilating $inst SST"
-#   fi
-#done
+   sed -e '/ICEC_emcice_JO/{r '${RUNDIRC}'/yaml/icec.cat_l2.emc.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
+   cp 3dvartmp.yml ${yamlfile}
+   rm 3dvartmp.yml
+else
+   echo "Not assimilating emc icec"
+fi
+
+# Add ghrsst to Jo.
+listofsst="windsat_l3u.ghrsst \
+           gmi_l3u.ghrsst \
+           amsr2_l3u.ghrsst \
+           avhrrmta_l3u.nesdis \
+           avhrr19_l3u.nesdis"
+for sst_source in $listofsst; do
+   obsfile=$obsdatabase/ioda.sst.${sst_source}.${DA_SLOT_LEN}h.nc 
+   echo $obsfile
+   if [ -f $obsfile ]; then
+      echo "Adding $sst_source SST to Jo cost function"
+      case $sst_source in 
+         "avhrrmta_l3u.nesdis")
+            subsample=true
+            skip=500
+            ;;
+         "avhrr19_l3u.nesdis")
+            subsample=true
+            skip=500
+            ;;
+         "windsat_l3u.ghrsst")
+            subsample=true
+            skip=10
+            ;;
+      esac
+
+      if [ $subsample ]; then
+         echo "Subsampling $sst_source SST"
+         # TODO: Subsample elsewhere
+	 cp ${obsfile} ${obsdatabase}/ioda.sst.${sst_source}_LARGE.nc
+         module load nco
+         # Create record dim
+         ncks --mk_rec_dmn nlocs ${obsdatabase}/ioda.sst.${sst_source}_LARGE.nc ${obsdatabase}/sst-tmp.nc
+         # Subsample
+         ncks -F -d nlocs,1,,$skip ${obsdatabase}/sst-tmp.nc ${DATADIR}/ioda.sst.${sst_source}.nc
+         rm ${obsdatabase}/sst-tmp.nc
+         rm ${obsdatabase}/ioda.sst.${sst_source}_LARGE.nc   
+      else
+         ln -sf ${obsfile} ${DATADIR}/ioda.sst.${sst_source}.nc
+      fi
+      echo SST_${sst_source}_JO
+      sed -e '/SST_'${sst_source}'_JO/{r '${RUNDIRC}'/yaml/sst.'${sst_source}'.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
+      cp 3dvartmp.yml ${yamlfile}
+      rm 3dvartmp.yml
+   else
+     echo "Not assimilating $sst_source SST"
+   fi
+done
 
 # Add insitu profiles to Jo
 obsfile=$obsdatabase/ioda.profile.${DA_SLOT_LEN}h.nc 
