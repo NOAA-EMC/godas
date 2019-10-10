484b75bb8ffffc41abc252b05135237eac895d56
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2019-10-10 01:16:53 +0000

    prep script for icec

---
+++ scripts/icec_prep_obs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l 
+
+cd $DCOM_ROOT
+
+OUTFILE=ioda.icec.emcice.${DA_SLOT_LEN}h.nc             #Filename of the processed obs
+PREPROCobs=${IODA_ROOT}/${CDATE}/${OUTFILE}     #FullPath/Filename of preprocessed obs
+PROCobs=${ObsRunDir}/${OUTFILE}                 #FullPath/Filename of observations to be ingested
+
+#Check if the observations have been preprocessed.
+if [ -f "${PREPROCobs}" ]; then
+   echo
+   echo PreProcessed Observations are copied from "${PREPROCobs}" \
+        to ${PROCobs}
+   echo
+
+   cp -rf ${PREPROCobs} ${PROCobs}
+
+   return
+fi
