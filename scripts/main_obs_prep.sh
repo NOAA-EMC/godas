3ec953ec4a987cba8fc2341bb2fbed4438a3098a
Shastri Paturi <shastri.paturi@noaa.gov>
2019-10-11 15:59:04 +0000

    Scripts to convert marine Obs (ADT, Profile and SST) data - cleaned

--- scripts/main_obs_prep.sh
+++ scripts/main_obs_prep.sh
@@ -1,31 +1,58 @@
 #!/bin/bash -l
+
 echo 'main_obs_prep.sh starts'
 echo ${ROOT_GODAS_DIR}
 echo CDATE is $CDATE
 
-ObsRunDir=$RUNCDATE/Data/${CDATE}    #Path for observations to be ingested by DA
-mkdir -p ${ObsRunDir}
-
 # Prep absolute dynamic topography obs
 source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh 
+echo "ADT done"
 
 # Prep T&S profile obs
 source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
+echo "FNMOC done"
 
 # Prep ice concentration obs
-source ${ROOT_GODAS_DIR}/scripts/icec_prep_obs.sh
+#source ${ROOT_GODAS_DIR}/scripts/icec_prep_obs.sh
 
 # Prep sst obs
 ListOfSST="sst.windsat_l3u.ghrsst \
            sst.gmi_l3u.ghrsst \
            sst.amsr2_l3u.ghrsst
-           sst.avhrrmta_l3u.nesdis \
-           sst.avhrr19_l3u.nesdis \
            sst.viirs_l3u.nesdis "
 
 for SSTsource in $ListOfSST;do
-   ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
-                     -i ${SSTsource}
+    sat=''
+    echo SSTsource is $SSTsource
+    echo sat is $sat
+    source ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
+                     -i ${SSTsource} \
+                     -d $sat
+    mv $OUTDIR/ioda.${SSTsource}.${sat}.${DA_SLOT_LEN}h.nc \
+       $OUTDIR/ioda.${SSTsource}.${DA_SLOT_LEN}h.nc  
+    echo $SSTsource done
+done
+
+# AVHRR data
+SSTsource="sst.avhrr_l3u.nesdis"
+for sat in AVHRR19 AVHRRMTA; do
+   echo "sat is" $sat
+   source ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
+                     -i  ${SSTsource} \
+                     -d  $sat
+   mv $OUTDIR/ioda.${SSTsource}.${sat}.${DA_SLOT_LEN}h.nc \
+      $OUTDIR/ioda.sst.${sat}_l3u.nesdis.${DA_SLOT_LEN}h.nc  
+# Change filename to lowercase
+   cd $OUTDIR
+   y=ioda.sst.${sat}_l3u.nesdis.${DA_SLOT_LEN}h.nc 
+   lc=`echo $y  | tr '[A-Z]' '[a-z]'`
+   if [ $lc != $y ]; then
+      mv $y $lc
+   fi
+   echo $lc
+#
+   echo $sat done
 done
 
 echo main_obs_prep ends
+
--- scripts/sst_prep_obs.sh
+++ scripts/sst_prep_obs.sh
@@ -2,60 +2,41 @@
 
 while getopts "i:d:" opt; do
    case $opt in
-      i) sst_source=("$OPTARG");;
-      d) filename=("$OPTARG");;
+      i) SSTsource=("$OPTARG");;
+      d) sat=("$OPTARG");;
    esac
 done
 shift $((OPTIND -1))
 
-echo SST SOURCE = ${sst_source}
-
-
+echo DCOM_ROOT=$DCOM_ROOT
 cd $DCOM_ROOT
 
-ObsRunDir=$RUNCDATE/Data/${CDATE}    #TODO: Should not be needed here ...
-OUTFILE=ioda.${sst_source}.${DA_SLOT_LEN}h.nc   #Filename of the processed obs
-PREPROCobs=${IODA_ROOT}/${CDATE}/${OUTFILE}     #FullPath/Filename of preprocessed obs
-PROCobs=${ObsRunDir}/${OUTFILE}                 #FullPath/Filename of observations to be ingested
-
-
-echo "obsrundir: "${ObsRunDir}
-echo "obsrundir: "$RUNCDATE/Data/${CDATE}
-
-#Check if the observations have been preprocessed.
-if [ -f "${PREPROCobs}" ]; then
-   echo
-   echo PreProcessed Observations are copied from "${PREPROCobs}" \
-        to ${PROCobs}
-   echo
-
-   cp -rf ${PREPROCobs} ${PROCobs}
-
-   exit
-fi
-
 OBSDCOM=$DCOM_ROOT/${sst_source}/$PDY
+#set -x
+OBSDCOM=$DCOM_ROOT/${SSTsource}/$PDY
+echo OBSDCOM = $OBSDCOM
 if [ -d "$OBSDCOM" ]; then
    
    OUTDIR=${ROTDIR}/${CDATE}
    mkdir -p ${OUTDIR}
    
    cd $OBSDCOM
-   echo SST Observations from ${sst_source} for $PDY exist at `pwd`
-   
+   echo SST Observations from ${SSTsource} for $PDY exist at `pwd`
+
    s="${IODA_EXEC}/gds2_sst2ioda.py -i "
-   for files in `ls ${filename}*.nc`; do
+   for files in `ls *${sat}*.nc`; do
       s+=" $OBSDCOM/${files} "
    done
    
-   s+=" -o ${OUTDIR}/ioda.${sst_source}.${DA_SLOT_LEN}h.nc -d ${CDATE}"
-
+   s+=" -o ${OUTDIR}/ioda.${SSTsource}.${sat}.${DA_SLOT_LEN}h.nc -d ${CDATE}"
    eval ${s}
+   echo $s
 
 else
    
    set -x
-   echo There are no SST observations from ${sst_source} for ${CDATE}  
+   echo There are no SST observations from ${SSTsource} for ${PDY}  
    set +x
 
 fi
+
