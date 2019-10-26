865c9805265b89342d68534ccdc1b03bbf1fb918
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2019-10-26 12:27:08 +0000

    functional envar

--- scripts/prep_3denvar_yaml.sh
+++ scripts/prep_3denvar_yaml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #
-# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # 
-#                       Unix Script Documentation 
-# 
+# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * #
+#                       Unix Script Documentation
+#
 # Script Name        :
 #
 # Script Description :
@@ -13,24 +13,35 @@
 #
 # History Log        :
 #
-# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # 
+# * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * #
 # set -x
 
 echo '#=================================================================#'
-echo '#                    prep_3dvar_yaml.sh starts                    #'
+echo '#                    prep_3denvar_yaml.sh starts                  #'
 echo '#                                                                 #'
 
-while getopts "i:d:n:" opt; do
+while getopts "i:m:d:n:" opt; do
    case $opt in
       i) yamlfile=("$OPTARG");;
+      m) yamlmember=("$OPTARG");;
       d) RUNDIR=("$OPTARG");;
+      n) Ne=("$OPTARG");;
    esac
 done
 shift $((OPTIND -1))
 
 cd ${RUNDIR}
 
-## Set date for 3DVAR
+echo $membertemplate
+for (( mbr=1; mbr<=${Ne} ; mbr++ )); do
+    echo "I am member "$mbr
+    cp $yamlmember tmpl.txt
+    sed -i "s/ENS_NUM/${mbr}/g" tmpl.txt
+    sed -e '/ENSEMBLE_MEMBERS/ {' -e 'r tmpl.txt' -e 'd' -e '}' -i ${yamlfile}
+    rm tmpl.txt
+done
+
+## Set date
 sed -i "s/WINDOW_BEGIN/${window_begin}/g" ${yamlfile}
 sed -i "s/WINDOW_LENGTH/${window_length}/g" ${yamlfile}
 sed -i "s/BKG_DATE/${bkg_date}/g" ${yamlfile}
@@ -43,6 +54,5 @@ ${ROOT_GODAS_DIR}/scripts/SetupJoCostFunction.sh \
       -d $RUNDIR
 
 echo '#                                                                 #'
-echo '#                      prep_3dvar_yaml.sh ends                    #'
+echo '#                      prep_3denvar_yaml.sh ends                  #'
 echo '#=================================================================#'
- 
