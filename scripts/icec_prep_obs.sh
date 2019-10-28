008bf141ecabe9d71faa5e7e8d19efab27225287
Yan Hao <yan.hao@noaa.gov>
2019-10-28 20:07:29 +0000

    Modified icec_prep_obs.sh

--- scripts/icec_prep_obs.sh
+++ scripts/icec_prep_obs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l 
 
 cd $DCOM_ROOT
-
-OUTFILE=ioda.icec.cat_l2.emc.${DA_SLOT_LEN}h.nc #Filename of the processed obs
+OBSDCOM=$DCOM_ROOT/icec_l2.emc                   #FullPath of raw obs
+OUTFILE=ioda.icec.cat_l2.emc.${DA_SLOT_LEN}h.nc  #Filename of the processed obs
 PREPROCobs=${IODA_ROOT}/${CDATE}/${OUTFILE}     #FullPath/Filename of preprocessed obs
 PROCobs=${ObsRunDir}/${OUTFILE}                 #FullPath/Filename of observations to be ingested
 
@@ -22,10 +22,23 @@ if [ -f "${PREPROCobs}" ]; then
    rm ${ObsRunDir}/icec-tmp.nc
    rm ${ObsRunDir}/ioda.icec.cat_l2.emc_LARGE.nc
 
-   sed -e '/ICEC_emcice_JO/{r '${RUNDIRC}'/yaml/icec.cat_l2.emc.yml' -e 'd}' ${yamlfile}> 3dvartmp.yml 
-   cp 3dvartmp.yml ${yamlfile}
-   rm 3dvartmp.yml
-
-
    return
+fi
+# Check if the raw observations exist and process.
+if [ -d "$OBSDCOM" ]; then
+
+   cd $OBSDCOM
+   n=`ls *${PDY}.nc |wc -l`
+
+   if [ $n -gt 0 ]; then 
+      s="${IODA_EXEC}/emc_ice2ioda.py -i "
+      for files in `ls *${PDY}.nc`; do
+        s+=" $OBSDCOM/${files} "
+      done
+      s+=" -o ${PROCobs} -d ${CDATE}"
+      eval ${s}
+   else
+      echo There are no ICEC observations for ${CDATE}
+   fi
+
 fi
