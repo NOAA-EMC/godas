14dc237a0cf453f5d9142ad588d98e372c35275b
Shastri Paturi <shastri.paturi@noaa.gov>
2019-11-07 16:04:43 -0500

    Scripts to convert marine Obs (ADT, Profile and SST) data - cleaned (#78)
    The objective of this PR is to separate all the observations in different files according to the provider, level, and sensor. 
    
    In order to achieve this, there are changes to the Observation Preparation Scripts, the DA Preparation scripts and the yaml files for the UFO filters used for each observation type. 
    
    This way, it's possible to tune the filters for each observation type, provider and sensor.
    
    * Scripts to convert marine Obs (ADT, Profile and SST) data are cleaned
    
    * Added WORKFLOW necessary lines of code and removed redundant checks to the scripts
    
    * Added the IODA-converter scripts for ADT, SST, and profiles
    
    * Removed echo statements used for testing purposes. The script converts the ADT data ti IODA format and splits them based on the satellite name. It also converts the SST data to the IODA format and splits the AVHRR data into the instrument name (AVHRR19, AVHRRMTA). The IODA converted filenames follow the naming convention
    
    * Removed echo statement used for testing purposes
    
    * Removed echo statement used for testing purposes
    
    * Modified the scripts for ADT, FNMOC, and SST to check for preprocessed obs and, if not process the raw obs. No thinning of SST, yet
    
    * Updated the ADT, FNMOC & SST scripts
    
    * Removed the debugging echoes and empty lines
    
    * Modified the scripts to process pre-staged obs correctly
    
    * Added comments in the SST processing script, as needed to explain the process and created a function for thinning
    
    * Fixed bug in the thinning function
    
    * Removed the DA_SLOT_WINDOW variable from the obs filename during linking
    
    * Corrected the name for SST obs file to include default window size

--- scripts/main_obs_prep.sh
+++ scripts/main_obs_prep.sh
@@ -1,32 +1,53 @@
 #!/bin/bash -l
+
 echo 'main_obs_prep.sh starts'
 echo ${ROOT_GODAS_DIR}
 echo CDATE is $CDATE
+
 
 ObsRunDir=$RUNCDATE/Data/${CDATE}    #Path for observations to be ingested by DA
 mkdir -p ${ObsRunDir}
 
 # Prep absolute dynamic topography obs
-source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh 
+ADTsource=adt.nesdis
+for sat in j1 j2 c2                         # Placeholder to add more satellites
+do
+   source ${ROOT_GODAS_DIR}/scripts/adt_prep_obs.sh \
+                    -i ${ADTsource} \
+                    -d ${sat}
+done
 
 # Prep T&S profile obs
 source ${ROOT_GODAS_DIR}/scripts/fnmoc_prep_obs.sh
 
 # Prep ice concentration obs
-source ${ROOT_GODAS_DIR}/scripts/icec_prep_obs.sh
+#source ${ROOT_GODAS_DIR}/scripts/icec_prep_obs.sh
 
 # Prep sst obs
 ListOfSST="sst.windsat_l3u.ghrsst \
            sst.gmi_l3u.ghrsst \
-           sst.amsr2_l3u.ghrsst
-           sst.avhrrmta_l3u.nesdis \
-           sst.avhrr19_l3u.nesdis \
-           sst.viirs_l3u.nesdis "
+           sst.amsr2_l3u.ghrsst \
+           sst.viirs_l3u.nesdis \
+           sst.avhrr_l3u.nesdis"
 
-for SSTsource in $ListOfSST;do
-   ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
+for SSTsource in $ListOfSST
+do
+   if [ "$SSTsource" == "sst.avhrr_l3u.nesdis" ]; then
+      for instr in avhrr19 avhrrmta; do
+          SSTsource=sst.${instr}_l3u.nesdis
+          source ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
                      -i ${SSTsource}
+
+          echo preprocessing of $SSTsource ${instr} done
+      done
+   else
+      source ${ROOT_GODAS_DIR}/scripts/sst_prep_obs.sh \
+                     -i ${SSTsource} 
+
+      echo preprocessing of $SSTsource done
+   fi
 done
+echo
 
 echo main_obs_prep ends
 
