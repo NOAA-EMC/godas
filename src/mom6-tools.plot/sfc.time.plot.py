80db56315f9e4b53163db47d92be28d61a1bc6d4
Jong Kim <jkbk2004@gmail.com>
2020-01-07 16:04:50 +0000

    updated sfc.time average script: less hard coded

--- src/mom6-tools.plot/sfc.time.plot.py
+++ src/mom6-tools.plot/sfc.time.plot.py
@@ -5,47 +5,89 @@ sys.path.append(os.path.dirname(file_path))
 from mom6_tools.MOM6grid import MOM6grid
 from mom6_tools.latlon_analysis import time_mean_latlon
 import matplotlib.pyplot as plt
+import xarray as xr
 import argparse
+import datetime
+import re
 import warnings
-warnings.filterwarnings("ignore")
-class args:
-  pass
 
-# required arg                                                                                                                      
-parser = argparse.ArgumentParser()
-parser.add_argument('-grid', required=True)
-parser.add_argument('-data', nargs='*', type=str, required=True)
-args = parser.parse_args()
+def get_ocn_hours(filename,ref_date):
+  ymd  = re.search(r'\d{4}_\d{2}_\d{2}', filename)
+  ymdh = re.search(r'\d{4}_\d{2}_\d{2}_\d{2}', filename)
 
-print(f'Loading grid... {args.grid}')
-print(f'Loading data... {args.data}')
+  ymd_=format(ymd.group(0))
+  ymd_dform=ymd_.replace('_', "-")
 
-# case name
-case_name = 'Test_data'
+  ymdh_=format(ymdh.group(0))
+  hour_=ymdh_.replace(ymd_+'_', "")
 
-# initial and final years for computing time mean
-year_start = 3
-year_end = 195
-xlabel='hours since 2012-01-01 00'
+  dateform=datetime.datetime.strptime(ymd_dform+' 00:00','%Y-%m-%d %H:%M').date()
+  delta = dateform - ref_date
+  filename_hours= delta.days*24+int(hour_)
 
-# variables to be processed
-variables = ['SST','SSH']
+  return filename_hours
 
-# Put your name and email address below
-author = 'Jong Kim (jong.kim@noaa.gov)'
-######################################################
+if __name__ == "__main__":
+  warnings.filterwarnings("ignore")
+  class args:
+    pass
 
-args.infile = args.data
-args.year_start = year_start
-args.year_end = year_end
-args.case_name = case_name
-args.variables = variables
-args.xlabel = xlabel
-args.savefigs = True
-args.time_series = True
+  # required arg  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-grid', required=True)
+  parser.add_argument('-data', nargs='*', type=str, required=True)
+  args = parser.parse_args()
 
-# load mom6 grid
-grd = MOM6grid(args.grid)
-grd.area_t=grd.Ah
-# plot time averages. If variables is NOT specified, all 2D variables will be plotted.
-time_mean_latlon(args,grd)
+  print(f'Loading grid... {args.grid}')
+  print(f'Loading data... {args.data}')
+
+  # case name
+  case_name = ''
+
+  # initial and final years for computing time mean
+  nc = xr.open_mfdataset(args.data[0], decode_times=False)
+  year_start=nc['time'].values
+  nc = xr.open_mfdataset(args.data[len(args.data)-1], decode_times=False)
+  year_end=nc['time'].values
+
+  # set ref date and time xlabel
+  ref_date='2011-10-01 00:00'
+  xlabel  ='hours since '+ref_date
+ 
+  # variables to be processed
+  variables = ['SST','SSH']
+
+  # Put your name and email address below
+  author = 'Jong Kim (jong.kim@noaa.gov)'
+  ######################################################
+
+  # set args to be use in making plots
+  args.infile = args.data
+  args.year_start = year_start
+  args.year_end = year_end
+  args.case_name = case_name
+  args.variables = variables
+  args.xlabel = xlabel
+  args.savefigs = True
+  args.time_series = True
+  args.filename_times = []
+  args.ref_date = datetime.datetime.strptime(ref_date,'%Y-%m-%d %H:%M').date()
+
+  # load mom6 grid
+  grd = MOM6grid(args.grid)
+  grd.area_t=grd.Ah
+
+  # set data and fig file path 
+  head, tail = os.path.split(args.data[0])
+  save_path = str(head + '/time_mean')
+  if not os.path.isdir(save_path): os.makedirs(save_path)
+  args.savefigs_path = save_path
+
+  # set external time stamp from data filename
+  for filename in args.data:
+    filename_hours = get_ocn_hours(filename, args.ref_date)
+    args.filename_times.append(filename_hours)
+
+  # do time mean plots
+  time_mean_latlon(args,grd)
+
