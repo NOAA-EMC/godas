c3d0ffc8ae00aaace099dc994b32f1c21091e0d2
flampouris <stylianos.flampouris@noaa.gov>
2020-01-13 08:18:09 -0500

    Feature/update setup (#142)
    * Updates for the setup_godas script
    
    * Update README.md
    
    * Syntax fix
    
    * Update test.setup_godas.py
    
    * Addressing Reviewers comments
    
    * Better use of if statement

--- test/test.setup_godas.py
+++ test/test.setup_godas.py
@@ -9,7 +9,7 @@ import io
 import shutil
 import shlex, subprocess
 import time
-import pwd
+import pathlib
 
 def make_dir(path):
     if os.path.isdir(path): delete_dir(path) 
@@ -62,41 +62,48 @@ def check_job_status(filename):
     for index in range(len(jobtask)):
         if jobstate[index] == 'SUCCEEDED':
             count_success += 1
-            print (jobcycle[index],jobtask[index],jobstate[index],jobtries[index])
+            print ( jobcycle[index],jobtask[index],jobstate[index],jobtries[index] )
         if jobstate[index] == 'SUBMITTING':
             count_submit = 1
             lapse_time = 60.*6
-            print (jobcycle[index],jobtask[index],jobstate[index],jobtries[index])
+            print ( jobcycle[index],jobtask[index],jobstate[index],jobtries[index] )
         if jobstate[index] == 'QUEUED':
             count_qued = 1
             lapse_time = 60.*6
-            print (jobcycle[index],jobtask[index],jobstate[index],jobtries[index])
+            print ( jobcycle[index],jobtask[index],jobstate[index],jobtries[index] )
         if count_success == len(jobtask): alljobs_success = True
 
     return alljobs_success, lapse_time
 
 if __name__ == '__main__':
     #1. Read directory paths, build/ctest option, and workflow name ------------
-    try:  
-        CLONE_DIR=os.environ.get('CLONE_DIR')
-    except KeyError:
-        print ("Please set the environment variable CLONE_DIR")
-        sys.exit(1)
-
     with open('test.setup_godas.yaml') as input_file:
          input = yaml.load(input_file)
+    
+    if 'CLONE_DIR' is os.environ:
+         CLONE_DIR=os.environ.get('CLONE_DIR')
+         print ( 'The CLONE_DIR is set as system variable' )
+    else:
+         CLONE_DIR = input.get('CLONE_DIR')
+    
+    print (CLONE_DIR)
     PROJECT_DIR = input.get('PROJECT_DIR')
     WORKFLOW_NAME = input.get('WORKFLOW_NAME')
     SKIP_BUILD= input.get('SKIP_BUILD')
-
+    FIX_SCRUB= input.get('FIX_SCRUB')
+    SCRUB= input.get('SCRUB')
     USER = os.getenv('USER')
-
+    os.environ["CLONE_DIR"] = CLONE_DIR
     path = os.getcwd()
+    
     print ("----- Current work directory is %s" % path)
 
-    if not SKIP_BUILD :    
+    if not SKIP_BUILD :
+        if os.path.isdir(CLONE_DIR): delete_dir(CLONE_DIR)
+    
         #1. Setup GODAS system at CLONE_DIR ------------------------------------
         print ('-----------Setup GODAS system at:',CLONE_DIR)
+        os.system("git clone https://github.com/NOAA-EMC/godas.git "+CLONE_DIR)
         os.chdir(CLONE_DIR)
         os.system("git submodule update --init --recursive")
 
@@ -107,6 +114,7 @@ if __name__ == '__main__':
 
         #2. Clone and build soca system inside CLONE_DIR------------------------
         print ('-----------Clone and build SOCA system at:',CLONE_DIR)
+        os.system("git clone --branch release/stable-nightly https://github.com/JCSDA/soca-bundle.git "+CLONE_DIR+"/src/soca-bundle")
         build_dir=CLONE_DIR+'/build'
         make_dir(build_dir); os.chdir(build_dir)
 
@@ -119,6 +127,7 @@ if __name__ == '__main__':
         #3. Clone and build the UMD-LETKF --------------------------------------
         os.chdir(CLONE_DIR)
         print ('-----------Clone and build the UMD-LETKF at:',CLONE_DIR)
+        os.system("git clone --recursive https://github.com/NOAA-EMC/UMD-LETKF.git ./src/letkf")
         os.chdir(CLONE_DIR+"/src/letkf")
         os.system("git submodule update --init --recursive")
         make_dir(CLONE_DIR+"/build/letkf")
@@ -129,7 +138,12 @@ if __name__ == '__main__':
         dst_letkf=CLONE_DIR+'/build/bin/letkfdriver'
         os.symlink(src_letkf,dst_letkf)
 
-    #4. Preparing the workflow and rocoto run case -----------------------------
+        #4. Preparing the mom6tools
+        src_fl=CLONE_DIR+'/src/mom6-tools.plot/*.py '
+        des_fl=CLONE_DIR+'/build/bin/ '
+        os.system('cp '+src_fl+des_fl)
+
+    #5. Preparing the workflow and rocoto run case -----------------------------
     os.chdir(CLONE_DIR+"/workflow")
     shutil.copyfile('user.yaml.default', 'user.yaml')
     subprocess.call(["sed", "-i",  '/PROJECT_DIR/c\  PROJECT_DIR: '+PROJECT_DIR, "user.yaml"])
@@ -138,20 +152,24 @@ if __name__ == '__main__':
 
     if os.path.isdir(PROJECT_DIR): delete_dir(PROJECT_DIR)
     make_dir(PROJECT_DIR)
-    user_id=pwd.getpwuid( os.getuid() ).pw_name
-    comrot_dir='/scratch1/NCEPDEV/stmp2/'+user_id+'/comrot/'+WORKFLOW_NAME
+    comrot_dir='/scratch1/NCEPDEV/stmp2/'+USER+'/comrot/'+WORKFLOW_NAME
+    
+    if FIX_SCRUB :
+        subprocess.call(["sed", "-i",  '/FIX_SCRUB:/c\  FIX_SCRUB: True', "user.yaml"])
+        subprocess.call(["sed", "-i",  '/SCRUB: none/c\  SCRUB: '+SCRUB, "user.yaml"])       
+        comrot_dir=SCRUB+USER+'/comrot/'+WORKFLOW_NAME
+    
     if os.path.isdir(comrot_dir): delete_dir(comrot_dir)
 
     crow_path = CLONE_DIR+"/workflow/CROW"
     os.chdir(crow_path)
-    
     subprocess.check_call(['bash','-c','./setup_case.sh -p HERA ../cases/3dvar.yaml '+WORKFLOW_NAME])
     subprocess.check_call(['bash','-c','./make_rocoto_xml_for.sh '+PROJECT_DIR+'/'+WORKFLOW_NAME])
 
     run_dir = PROJECT_DIR+'/'+WORKFLOW_NAME
     os.chdir(run_dir)
-
-    #5. Runing the rocoto workflow and checking run status ---------------------
+    
+    #6. Runing the rocoto workflow and checking run status ---------------------
     max_runs   = 16*2
     count_runs = 0
     while (count_runs < max_runs):
@@ -167,8 +185,7 @@ if __name__ == '__main__':
         time.sleep(lapse_time) # pause 3 or 6 minutes
         count_runs += 1
 
-    #6. Checking the log file to validate run result----------------------------
+    #7. Checking the log file to validate run result----------------------------
     run_dir = PROJECT_DIR+'/'+WORKFLOW_NAME+'/log'
-    for filename in find_files(run_dir, '*.log'):
+    for filename in pathlib.Path('run_dir').glob('*.log'):
         check_log(filename,'exit code 0:0')
-
