9a252699e2a2ce7a84b92e2acc9a8c9b36fb8cfb
flampouris <stylianos.flampouris@noaa.gov>
2020-04-16 14:04:57 -0400

    Porting GODAS to Orion - Part1 (#167)
    * Update the link.sh to support installation of the GODAS on orion
    
    * Updated modulefiles for the jedi
    
    * WIP deployment of godas to orion
    
    * testing the updates at hera

--- src/link.sh
+++ src/link.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
-set -xu
+set -ue
 
 topdir=$(pwd)
 echo $topdir
 
 if [ $# -eq 1 ]; then
-model=$1
+  model=$1
 fi
 model=${model:-"godas"}
 
+if [ $# -eq 2 ]; then
+  hpc=$2
+fi
+
 if [ $model = "godas" ]; then
+  case $hpc in
 
-#if on hera:  (should put on hpss eventually too)
-ln -sf /scratch2/NCEPDEV/marineda/godas_input/FIX/* $topdir/../fix/
+  hera) 
+     ln -sf /scratch2/NCEPDEV/marineda/godas_input/FIX/* $topdir/../fix/
+     ;;
 
+  orion)
+     ln -sf /work/noaa/marine/marineda/godas_input/FIX/* $topdir/../fix/
+     ;;
 
+  *)
+  #TODO generalize  (should put on hpss eventually too)
+    echo "The HPC is unknown, please add the location of the FIX files, exiting..."
+    exit
+esac
 fi
