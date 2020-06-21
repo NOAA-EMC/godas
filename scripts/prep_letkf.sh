5b3198ddefa160e9bea850e4ab3ebd684b33eb34
Jong Kim <jkbk2004@gmail.com>
2020-06-21 22:57:07 +0000

    updated prep_letkf: yaml, input files

--- scripts/prep_letkf.sh
+++ scripts/prep_letkf.sh
@@ -34,17 +34,29 @@ cd ${LETKFDIR}
 #---------------------------------------------------
 # a. grid information
 
-cp $SOCA_STATIC/soca_gridspec.nc ${LETKFDIR}
-
 cp $SOCA_STATIC/INPUT/layer_coord.nc            \
    ${LETKFDIR}/Vertical_coordinate.nc
-
 
 # b. yaml file for UMD-LETKF
 mkdir -p ${LETKFDIR}/yaml
 cp -r $SOCA_CONFIG/letkf.yml ${LETKFDIR}/yaml/letkf.yaml
 cp -r $SOCA_CONFIG/gridgen.yml ${LETKFDIR}/yaml/gridgen.yml
 
+# Copy SOCA static files into RUNCDATE                                                                      
+#---------------------------------------------------                                                        
+cp -r $SOCA_STATIC/* ${LETKFDIR}
+
+# Adjust date in input-mom6.nml                                                                                  
+echo "CDATE is $CDATE" 
+cp ${LETKFDIR}/input-ymd.nml $LETKFDIR/input-mom6.nml
+YYYY=$(echo  $CDATE | cut -c1-4)
+MM=$(echo $CDATE | cut -c5-6)
+DD=$(echo   $CDATE | cut -c7-8)
+HH=$(echo  $CDATE | cut -c9-10)
+sed -i "s/YYYY/${YYYY}/g" $LETKFDIR/input-mom6.nml
+sed -i "s/MM/${MM}/g" $LETKFDIR/input-mom6.nml
+sed -i "s/DD/${DD}/g" $LETKFDIR/input-mom6.nml
+sed -i "s/HH/${HH}/g" $LETKFDIR/input-mom6.nml
 
 # Prep yaml for letkf
 #----------------------------------------------------
