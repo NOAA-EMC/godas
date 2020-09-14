f240b0c655ab63166174f5c4c2d6a735ed00bd7e
clouden90 <clouden90@github.com>
2020-09-14 10:53:06 -0500

    Update ufs scratch files

--- ufs/CFG_DIR/0.25deg/input.nml.sh
+++ ufs/CFG_DIR/0.25deg/input.nml.sh
@@ -15,58 +15,18 @@ if [[ "$FCST_RESTART" == 1 ]]; then restart='r'; fi
 
 cat <<EOF
 
- &diag_manager_nml
- /
-
- &ocean_solo_nml
-            months = 0
-            days   = 0
-            hours  = $FCST_LEN,
-            date_init = $(date -u "+%Y,%m,%d,%H" -d "${FCST_START_TIME}"),0,0,
-            minutes = 0
-            seconds = 0
-            calendar = 'NOLEAP' /
-
-
+&fms_nml
+            clock_grain='ROUTINE'
+            clock_flags='NONE'
+            domains_stack_size = 5000000
+            stack_size =0
+/
  &MOM_input_nml
          output_directory = 'OUTPUT',
          input_filename = '$restart'
-         restart_input_dir = 'RESTART_IN',
-         restart_output_dir = 'RESTART',
+         restart_input_dir = 'RESTART_IN/',
+         restart_output_dir = 'RESTART/',
          parameter_filename = 'MOM_input',
-                              'MOM_override'
-/
-
- &data_override_nml
- /
-
- &fms_io_nml
-         fms_netcdf_restart=.true.
-         threading_read='multi'
-         max_files_w=100
-         checksum_required=.false.
- /
-
- &fms_nml
-       clock_grain='MODULE'
-       domains_stack_size = 9552960 
-       clock_flags='SYNC' /
-
- &ice_albedo_nml
-      t_range = 10. /
-
- &ice_model_nml
-           /
-
- &monin_obukhov_nml
-            neutral = .true. /
-
- &ocean_albedo_nml
-      ocean_albedo_option = 5 /
-
- &sat_vapor_pres_nml
-      construct_table_wrt_liq = .true.,
-      construct_table_wrt_liq_and_ice = .true. /
-
+                              'MOM_override' /
 
 EOF
