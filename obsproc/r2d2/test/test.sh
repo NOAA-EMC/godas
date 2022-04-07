f4676e2b076683d8bcbac249c46ee59f9122640b
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2022-04-07 15:20:24 -0500

    fixed ctest

--- obsproc/r2d2/test/test.sh
+++ obsproc/r2d2/test/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
-export R2D2_CONFIG=$PWD/r2d2_config.yaml
-../bin/soca_store_obs.py --start 20150101 \
-                         --end 20150102 \
-                         --source ./obs-2-move \
+../bin/soca_store_obs.py --start 2015-01-01T00:00:00Z \
+                         --end 2015-01-03T00:00:00Z \
+                         --source_dir ./obs-2-move \
+                         --source_file ymd \
                          --provider jcsda_soca \
                          --experiment benchmark_v2 \
                          --obstype adt \
