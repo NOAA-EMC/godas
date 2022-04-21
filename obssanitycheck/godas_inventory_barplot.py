df98184959a8eb087627f6362cd224e95c6b9407
mjhossen <mjhossen55@gmail.com>
2022-04-21 08:57:29 -0500

    apr21

---
+++ obssanitycheck/godas_inventory_barplot.py
@@ -0,0 +1,192 @@
+from netCDF4 import Dataset, num2date, date2num
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from datetime import datetime, timedelta
+from os.path import exists
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+import matplotlib.gridspec as gridspec
+import sys
+from glob import glob
+import plotly.express as px
+import pandas as pd
+import plotly.io as pio
+import yaml
+
+class ObsInventory:
+    def __init__(self,args=None):
+        self.folder = args.path 
+        self.start_date = args.start
+        self.end_date = args.end
+        self.year=self.start_date[0:4]
+        self.args=args
+
+    def inventory_p1d(self, folder=None):
+        if folder== None:
+            folder=self.folder
+        start_date=datetime.strptime(self.start_date, '%Y%m%d').date()
+        end_date=datetime.strptime(self.end_date, '%Y%m%d').date()
+        plf_avail_date={}
+        for file_descriptor in self.args.file_descriptor:
+            #print('des',file_descriptor)
+            current_date=start_date
+            while current_date < end_date:
+                ddir=folder+'/'+str(current_date)
+                list_of_files=glob(ddir+'/*%s*.nc*'%(file_descriptor))
+                if not list_of_files:
+                    ind=0
+                else:
+                    ind=1
+                plf_avail_date.setdefault(file_descriptor, []).append([ind, current_date])                    
+                current_date=current_date+timedelta(days=1)
+            plf_avail_date.setdefault(file_descriptor, []).append([0, end_date])                    
+        self.plf_avail_date=plf_avail_date
+        self.plot_p1d(plf_avail_date)
+    
+    def plot_p1d(self, plf_data):
+        listd=[]
+        for plf in plf_data.keys():
+            data=plf_data[plf]
+            data=np.array(data)
+            ind=data[:,0]
+            avail_dates=data[:,1]
+            is_key=1
+            current=avail_dates[0]
+            end_date=avail_dates[-1]
+            start=current
+            end=current
+            i=0
+            while current<end_date:
+                current=avail_dates[i]
+                if ind[i]==1:
+                    if is_key==1:
+                        start=current
+                        is_key=0
+                if ind[i]==0:
+                    if is_key==0:
+                        end=current
+                        is_key=1
+                        if (end-start).days >2:
+                            listd.append(dict(File_descriptor="%s"%plf, Start='%s'%start, Finish='%s'%end))
+                        start=avail_dates[0]
+                        end=avail_dates[0]
+                i=i+1 
+            if (end-start).days >2:
+                listd.append(dict(Obs_type="%s"%plf, Start='%s'%start, Finish='%s'%end))
+
+        df= pd.DataFrame(listd)
+        #print('data frame', listd)
+        fig = px.timeline(df, x_start="Start", x_end="Finish", y="File_descriptor", 
+            facet_row_spacing=.1, template="simple_white",
+            title="gdas_marine s2s_v1 P1D files")
+        fig.update_traces(width=0.7, marker=dict(color='lightgrey', 
+            line=dict(width=1,  color='green')))
+        fig.update_yaxes(showgrid=True, linewidth=2, mirror=True, title_standoff = 5)
+        fig.update_xaxes(showgrid=True, linewidth=2, mirror=True)
+        fig.update_layout(title_x=0.55, yaxis_title=None, margin=dict(l=20, r=20, t=25, b=20))
+        pio.write_image(fig, 'Fig_inventory_P1D_%s-%s.png'%(self.start_date[:4], self.end_date[:4]), format='png')
+
+    def inventory_pt10m(self, folder=None):
+        if folder== None:
+            folder=self.folder
+        start_date=datetime.strptime(self.start_date, '%Y%m%d%H%M')
+        end_date=datetime.strptime(self.end_date, '%Y%m%d%H%M')
+        
+        plf_avail_date={}
+        for file_descriptor in self.args.file_descriptor:
+            current_date=start_date
+            while current_date < end_date:
+                ddir=folder+'/%s'%current_date.date()
+                list_of_files=glob(ddir+'/*%s*%s*.nc*'%(file_descriptor, current_date.time()))
+                if not list_of_files:
+                    ind=0
+                else:
+                    ind=1
+                plf_avail_date.setdefault(file_descriptor, []).append([ind, current_date])                    
+                current_date=current_date+timedelta(minutes=10)
+            plf_avail_date.setdefault(file_descriptor, []).append([0, end_date])                    
+        self.plf_avail_date=plf_avail_date
+        self.plot_pt10m(plf_avail_date)
+        
+    def plot_pt10m(self, plf_data):
+        listd=[]
+        for plf in plf_data.keys():
+            data=plf_data[plf]
+            data=np.array(data)
+            ind=data[:,0]
+            avail_dates=data[:,1]
+            is_key=1
+            current=avail_dates[0]
+            end_date=avail_dates[-1]
+            start=current
+            end=current
+            i=0
+            while current<end_date:
+                current=avail_dates[i]
+                if ind[i]==1:
+                    if is_key==1:
+                        start=current
+                        is_key=0
+                if ind[i]==0:
+                    if is_key==0:
+                        end=current
+                        is_key=1
+                        if (end-start)/timedelta(minutes=1) >10:
+                            listd.append(dict(File_descriptor="%s"%plf, Start='%s'%start, Finish='%s'%end))
+                        start= avail_dates[i]
+                        end  = avail_dates[i]
+                i=i+1 
+            if (end-start)/timedelta(minutes=1) >10:
+                listd.append(dict(Obs_type="%s"%plf, Start='%s'%start, Finish='%s'%end))
+
+        df= pd.DataFrame(listd)
+        print('data frame', listd)
+        fig = px.timeline(df, x_start="Start", x_end="Finish", y="File_descriptor", 
+            facet_row_spacing=.1, template="simple_white",
+            title="gdas_marine s2s_v1 PT10M files")
+        fig.update_traces(width=0.5, marker=dict(color='lightgrey', 
+            line=dict(width=1,  color='green')))
+        fig.update_yaxes(showgrid=True, linewidth=2, mirror=True, title_standoff = 5)
+        fig.update_xaxes(showgrid=True, linewidth=2, mirror=True)
+        fig.update_layout(title_x=0.55, yaxis_title=None, margin=dict(l=20, r=20, t=25, b=20))
+        #fig.update_layout(yaxis=dict(ticktext=self.args.file_descriptor))
+        pio.write_image(fig, 'Fig_inventory_PT10M_%s-%s.png'%(self.start_date[:6], self.end_date[:6]), format='png')
+
+  
+if __name__ == '__main__':
+    description = """ Ex: godas_obssanitycheck.py -f adt_c2 -s start -e end -q frequency
+                      Suggestions: limit the range between 3-6 months for daily data
+                                for hourly or minutes data limit a day to few days       
+                  """
+    # Command line argument
+    parser = ArgumentParser(
+        description=description,
+        formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-s','--start',
+        help="start date (yearmmdd, yearmmddhhmn, ...)",
+        type=str, required=True)
+    parser.add_argument( '-e','--end',
+        help="end date in the format yearmmdd",type=str, required=True)
+    parser.add_argument('-y','--yaml',
+        help="Provide yaml file", type=str, required=False)
+
+    args = parser.parse_args()
+    if args.yaml is not None:
+        with open(args.yaml, 'r') as yaml_file:
+            config=yaml.safe_load(yaml_file)
+        args.file_descriptor=config['obs_types']
+        args.path=config['obs_dir']
+        args.step=config['step']
+    print(args)
+    obj_inventory=ObsInventory(args=args)
+
+    #obj_inventory.inventory_barplot(folder=args.path)
+    if len(args.start)==8 and args.step=='P1D': 
+        obj_inventory.inventory_p1d(folder=args.path)
+
+    elif len(args.start)>8 and args.step == 'PT10M':
+        obj_inventory.inventory_pt10m(folder=args.path)
+    else:
+        sys.exit('inconsistent information. Please check your date and yaml file')
+
+
--- obssanitycheck/godas_obssanitycheck.py
+++ obssanitycheck/godas_obssanitycheck.py
@@ -8,6 +8,9 @@ import matplotlib.dates as mdates
 import matplotlib.gridspec as gridspec
 import sys
 from glob import glob
+import plotly.express as px
+import pandas as pd
+import plotly.io as pio
 
 class ObsSanityCheck:
     def __init__(self,args=None):
@@ -17,7 +20,7 @@ class ObsSanityCheck:
         self.year=self.start_date[0:4]
         self.args=args
 
-    def extract_info_minutes(self, folder=None):
+    def extract_info_pt10m(self, folder=None):
         if folder is None:
             folder=self.folder
         file_descriptor=self.args.file_descriptor
@@ -46,94 +49,198 @@ class ObsSanityCheck:
             current_date=current_date+timedelta(minutes=10)
         self.plot_info(nlocs, minval, maxval, avail_dates, missing_hours, freq='hours')
 
-    def extract_info_daily(self, folder=None):
-        file_descriptor=self.args.file_descriptor
+
+    def extract_info_p1d(self, folder=None):
+        #file_descriptor=self.args.file_descriptor
         if folder== None:
             folder=self.folder
         start_date=datetime.strptime(self.start_date, '%Y%m%d').date()
         end_date=datetime.strptime(self.end_date, '%Y%m%d').date()
-        current_date=start_date
         missing_dates=[]
         nlocs=[]
         avail_dates=[]
-        minval=[]
-        maxval=[]
-        while current_date < end_date:
-            ddir=folder+'/'+str(current_date)
-            list_of_files=glob(ddir+'/*%s*.nc*'%file_descriptor)
-            if not list_of_files:
-                missing_dates.append(current_date)
-            else:
-                nloc, minn, maxx=self.find_param(list_of_files[0])
-                nlocs.append(nloc)
-                minval.append(minn)
-                maxval.append(maxx)
-                avail_dates.append(current_date)
-            current_date=current_date+timedelta(days=1)
-        self.plot_info(nlocs, minval, maxval, avail_dates, missing_dates, freq='dates')
-
-    def plot_info(self, nlocs, minval, maxval, avail_dates, missing_dates, freq=None):        
+        obsval=[]
+        obserr=[]
+        plf_data={}
+        plf_missdates={}
+        var_name=None
+        for plf in self.args.platform:
+            obstype=self.args.obstype
+            current_date=start_date
+            while current_date < end_date:
+                ddir=folder+'/'+str(current_date)
+                list_of_files=glob(ddir+'/*%s*%s*.nc*'%(obstype, plf))
+                if not list_of_files:
+                    plf_missdates.setdefault(plf, []).append(current_date)
+                else:
+                    nloc, minval, maxval=self.find_param(list_of_files[0], group='ObsValue')
+                    nloc, minerr, maxerr=self.find_param(list_of_files[0], group='ObsError')
+                    avail_dates.append(current_date)
+                    plf_data.setdefault(plf, []).append([nloc, minval, maxval, minerr, maxerr, current_date])                    
+                current_date=current_date+timedelta(days=1)
+        self.plf_data=plf_data
+        self.plf_missdates=plf_missdates
+        if len(plf_data) !=0 : 
+            self.plot_info(plf_data, plf_missdates, freq='dates')
+        else:
+            sys.exit('Nothing is found for the range of the date; check your database')
+    #def plot_info(self, nlocs, obsval, obserr, var_name, avail_dates, missing_dates, freq=None):        
+    def plot_info(self, plf_data, plf_missdates, freq=None):        
         
-        file_descriptor=self.args.file_descriptor
+        #file_descriptor=self.args.file_descriptor
         formatter = mdates.DateFormatter('%Y/%m/%d')
-        fig = plt.figure(tight_layout=True)
-        gs = gridspec.GridSpec(2, 2)
-        
-        # --- plot number of observations ---
-        ax = fig.add_subplot(gs[0, 0])
-        nlocs=[xx/1000 for xx in nlocs]
-        ax.plot_date(avail_dates, nlocs,'-og')
-        ax.title.set_text('No of observations')
-        ax.set_ylabel('No of obs')
+        fig = plt.figure(figsize=(7,8), tight_layout=True)
+        self.color_plf=['g','r','b','k','m','y']
+        self.units={'sea_ice_area_fraction':'\%', 'absolute_dynamic_topography': 'm', \
+            'ice_concentration':'\%', 'sea_surface_salinity':'psu', 'sea_water_salinity':'psu',
+            'sea_surface_temperature':'C' }
+
+        gs = gridspec.GridSpec(3, 3)
+        ax = fig.add_subplot(gs[0, 0:2])
+        self.plot_obscount(ax)
+        # --- Plot min/max value of a variable
+        ax = fig.add_subplot(gs[1, 0:2])
+        self.plot_obsvalue(ax)
+        # --- Plot min/max value of error 
+        ax = fig.add_subplot(gs[2, 0:2])
+        self.plot_obserror(ax)
+        # --- Write missing files ----
+        ax=fig.add_subplot(gs[:,2])
+        self.write_missing_on_plot(ax)
+
+        plt.suptitle("%s: %s-%s"%(self.args.obstype.upper(), self.start_date, self.end_date), y=.98, fontsize=14);
+        plt.savefig('Fig_%s_%s_mm-%s-%s.png'%(self.year, self.args.obstype, self.start_date[4:6], self.end_date[4:6]), bbox_inches='tight', pad_inches=0.1)
+   
+    def plot_obscount(self, ax):
+        nlocmax=[]
+        nlocmin=[]
+        i=0
+        for plf in self.args.platform:
+            if plf not in self.plf_data.keys():
+                continue 
+            data=self.plf_data[plf]
+            data=np.array(data)
+            nlocs=data[:,0]
+            avail_dates=data[:,5]
+            nlocs=[xx/1000 for xx in nlocs]
+            nlocmax.append(max(nlocs))
+            nlocmin.append(min(nlocs))
+            ax.plot_date(avail_dates, nlocs,'-o', color=self.color_plf[i], label='%s'%plf)
+            i=i+1
+
+        ax.legend(ncol=min(2,len(self.args.platform)), labelspacing = 0.2, loc=0)
+        ax.title.set_text('Obs Count ')
+        ax.set_ylabel('No of obs data (in thousand)')
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
         ax.set_xticklabels([])
         if len(nlocs) != 0:
-            ytkl= np.linspace(min(nlocs), max(nlocs), 5)
+            ytkl= np.linspace(min(nlocmin), max(nlocmax), 5)
             yticks=['%sk'%int(xx) for xx in ytkl]
             ax.set_yticks(ytkl)
             ax.set_yticklabels(yticks)
-
-        # --- Plot min/max value of a variable
-        ax = fig.add_subplot(gs[1, 0])
-        plt.title('Min and Max value of a variable')
-        plt.plot_date(avail_dates, minval,'-og', label='Min')
-        plt.plot_date(avail_dates, maxval,'-*r', label='Max')
-        ax.set_ylabel('Min/Max')
-        ax.set_xlabel('Dates')
-        ax.legend(loc=3)
-        ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
+ 
+    def plot_obserror(self, ax):
+        #plt.title('error of %s'%var_name)
+        if "_" in self.var_name:
+            varr_name=self.var_name.replace('_', " ")
+        plt.title('%s (%s)'%(varr_name, self.units[self.var_name]))
+        i=0
+        for plf in self.args.platform:
+            if plf not in self.plf_data.keys():
+                continue 
+            data=self.plf_data[plf]
+            data=np.array(data)
+            minerr=data[:,3]
+            maxerr=data[:,4]
+            avail_dates=data[:,5]
+            plt.plot_date(avail_dates, maxerr,'-s', markersize=6, color=self.color_plf[i], label='%s (max)'%plf)
+            plt.plot_date(avail_dates, minerr,'-o', markersize=6,  color=self.color_plf[i],label='%s (min)'%plf)
+            i=i+1
+        ax.set_ylabel('Obs Error')
+        ax.set_xlabel('dates')
+        plt.grid(True)
+        ax.legend(ncol=min(3,len(self.args.platform)), labelspacing = 0.2, loc=0)
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=15))
         ax.set_xticklabels([])
 
-        # --- Write missing files ----
-        ax=fig.add_subplot(gs[:,1])
-        md_len=len(missing_dates)
-        plt.title('Missing files (%s): %s'%(md_len, freq))
-        if md_len == 0:
-            plt.text(.12, .75, 'No missing files!', fontsize=13)
-        else:
-            xstep=0.01
-            ystep=0.95
-            for tx in missing_dates:
-                plt.text(xstep, ystep, '%s'%tx, fontsize=8)
-                ystep=ystep-0.05
-                if ystep < .005: 
-                    xstep=xstep+0.33
-                    ystep=0.95
-        
+    def plot_obsvalue(self, ax):
+        #var_name=self.fxn(var_name)
+        if "_" in self.var_name:
+            varr_name=self.var_name.replace('_', " ")
+        plt.title('%s (%s)'%(varr_name, self.units[self.var_name]))
+        i=0
+        for plf in self.args.platform:
+            if plf not in self.plf_data.keys():
+                continue 
+            data=self.plf_data[plf]
+            data=np.array(data)
+            minval=data[:,1]
+            maxval=data[:,2]
+            avail_dates=data[:,5]
+            plt.plot_date(avail_dates, maxval,'-s', markersize=6, color=self.color_plf[i], label='%s (max)'%plf)
+            plt.plot_date(avail_dates, minval,'-o', markersize=6, color=self.color_plf[i],label='%s (min)'%plf)
+            i=i+1
+        ax.set_ylabel('Obs Value')
+        plt.grid(True)
+        #ax.legend(ncol=len(self.args.platform), loc=0)
+        ax.legend(ncol=min(3,len(self.args.platform)), loc=0, fontsize=10, labelspacing=0.2)
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=15))
         ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        plt.suptitle("%s: %s-%s"%(file_descriptor, self.start_date, self.end_date), y=.96, fontsize=14);
-        plt.savefig('Fig_%s_%s.png'%(file_descriptor, self.year), bbox_inches='tight', pad_inches=0.1)
-
     # --- extract no of observations and min/max value of a varibale -----
+    def write_missing_on_plot(self, ax):
+            
+        #plt.title('Missing files (mm-dd)')
+        xstep=0.01
+        ystep=0.98
+        # First write "not files exist"
+        # then, write the "no missing files:
+        # then write the date when files are missing
+        for plf in self.args.platform:
+            if plf not in self.plf_data.keys():
+                plt.text(xstep, ystep, 'No files exist for %s!'%plf)
+                ystep=ystep-0.03
+                continue
+            if plf not in self.plf_missdates.keys():
+                plt.text(xstep, ystep, 'No missing files for %s!'%plf)
+                ystep=ystep-0.03
+                continue
+        
+        ystep=ystep-0.01
+        # write dates when files are missing for some platforms.
+        plt.text(xstep, ystep, 'Missing files (mm-dd)')
+        ystep=ystep-0.035
 
-    def find_param(self, fname):
+        yheight=ystep
+        for plf in self.args.platform:
+            if plf not in self.plf_data.keys():
+                continue
+            if plf not in self.plf_missdates.keys():
+                #ystep=ystep-0.05
+                continue
+            missing_dates=self.plf_missdates[plf]
+            
+            #ystep=ystep-0.05
+            plt.text(xstep, ystep, 'PF: %s'%plf, bbox=dict(facecolor='none', edgecolor='red'), fontsize=10)
+            for tx in missing_dates:
+                #print(tx, tx.day)
+                ystep=ystep-0.027
+                if ystep < .01: 
+                    xstep=xstep+0.355
+                    ystep=yheight
+                plt.text(xstep, ystep, '%02d-%02d |'%(tx.month, tx.day), fontsize=9)
+            ystep=ystep-0.05
+        ax.axis('off')
+
+    def find_param(self, fname, group='ObsValue'):
         args=self.args
         ncfile = Dataset(fname,'r')
         metadata_grp=ncfile.groups['MetaData']
-        var_grp=ncfile.groups['ObsValue']
-        var_name=list(var_grp.variables)
-        var_tmp=np.squeeze(var_grp.variables[var_name[0]][:])
+        var_grp=ncfile.groups[group]
+        var_names=list(var_grp.variables)
+        #print(var_names)
+        var_name=var_names[0]
+        var_tmp=np.squeeze(var_grp.variables[var_name][:])
+        self.var_name=var_name
         nlocs=len(var_tmp)
         minval=0
         maxval=0
@@ -142,9 +249,9 @@ class ObsSanityCheck:
             maxval=max(var_tmp)
         ncfile.close()
         return nlocs,  minval, maxval
-        
+  
 if __name__ == '__main__':
-    description = """ Ex: godas_obssanitycheck.py -f adt_c2 -s start -e end -q frequency
+    description = """ Ex: godas_obssanitycheck.py -t adt -p c2 j2 j3 sa 3a -s start -e end -q P1D
                       Suggestions: limit the range between 3-6 months for daily data
                                 for hourly or minutes data limit a day to few days       
                   """
@@ -152,33 +259,32 @@ if __name__ == '__main__':
     parser = ArgumentParser(
         description=description,
         formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-f', '--file_descriptor',
+    parser.add_argument('-t', '--obstype', help='provide obstype (adt, sst, sss, icec,...)',
         type=str, required=True)
+    parser.add_argument('-p', '--platform', help='provide platforms adt(c2, j2, ...), sst(noaa18, noaa19, ...), ...)',
+        type=str, nargs='+', required=True)
     parser.add_argument('-s','--start',
         help="start date (yearmmdd, yearmmddhhmn, ...)",
         type=str, required=True)
     parser.add_argument( '-e','--end',
         help="end date in the format yearmmdd",type=str, required=True)
-    parser.add_argument('-p','--path',
+    parser.add_argument('--path',
         help="path of the data dir", type=str, required=False)
-    parser.add_argument('-q', '--frequency', default='daily',
-        help="frequency for daily or minutes data", type=str, required=False)
+    parser.add_argument('-r', '--step', default='P1D',
+        help="resolution for the data, such as P1D or PT10M", type=str, required=False)
 
     args = parser.parse_args()
+    print(args)
     obj_sanity=ObsSanityCheck(args=args)
 
-    if args.frequency=='daily':
-        if len(args.start) != 8:
-            sys.exit('default frequency is daily. Please provide date as %Y%m%d or provide frequency for hourly/minutes')
+    if len(args.start)==8 and args.step=='P1D': 
         if args.path == None:        
-            args.path='/work/noaa/marine/marineda/r2d2/obs/jcsda_soca/ob/benchmark_v2/P1D'
-        obj_sanity.extract_info_daily(folder=args.path)
-
-    if args.frequency=='minutes' or args.frequency=='hourly':
-        if len(args.start) <= 8:
-            sys.exit('Please provide date as %Y%m%d%hh%mm')
+            args.path='/work/noaa/ng-godas/r2d2/gdas_marine/ob/s2s_v1/P1D'
+        obj_sanity.extract_info_p1d(folder=args.path)
+    elif len(args.start)>8 and args.step == 'PT10M':
         if args.path == None:
-            args.path='/work/noaa/marine/Guillaume.Vernieres/OBS/sprint1of4/convert2ioda/scratch/ioda_data'
-        obj_sanity.extract_info_minutes(folder=args.path)
-
+            args.path='/work/noaa/ng-godas/r2d2/gdas_marine/ob/s2s_v1/PT10M' 
+        obj_sanity.extract_info_pt10m(folder=args.path)
+    else:
+        sys.exit('inconsistent information. Please check your dates and step')
 
