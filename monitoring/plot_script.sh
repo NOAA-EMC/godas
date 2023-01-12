617f6d3ad5bb06bb5ed582a748c69fdb82dada6c
mjhossen <mjhossen55@gmail.com>
2023-01-12 14:33:49 -0600

    giomas

---
+++ monitoring/plot_script.sh
@@ -0,0 +1,6 @@
+mkdir -p plots
+yr=$1
+ddir=/work/noaa/ng-godas/marineda/validation/GIOMAS/$yr
+ddir=.
+python interpolate_icethickness_monthly.py -g soca_gridspec.nc -m 8 9 10 11 -s ${ddir}/heff.H${yr}.nc -o heff.G${yr}.nc # -p True -r True
+
